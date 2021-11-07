60945faeee69bd42e186941b1bd5e316e14869c8
mehmet sekeroglu <mehmetsekeroglu2019@gmail.com>
2021-11-07 18:14:38 +0100

    single page app-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,7 +25,14 @@ class Main extends Component {
   
  
   
-    render() {
+  render() {
+      
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
 
       const HomePage = () => {
         return(
@@ -42,6 +49,7 @@ class Main extends Component {
            <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact}/>
               <Redirect to="/home" />
           </Switch>
